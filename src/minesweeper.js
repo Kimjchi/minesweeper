ffbf4aec05f61b3d00de7e2d1778d34b2a96641f
kimjii <kimj77600@gmail.com>
2017-10-31 11:10:10 -0400

    Generate a board with bombs placed randomly

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -11,5 +11,23 @@ const generatePlayerBoard = (numberOfRows, numberofColumns) => {
 };
 
 const generateBombBoard = (numberOfRows, numberofColumns, numberOfBombs) => {
-  
+  let board = [];
+  for (let i = 0; i < numberOfRows; i++) {
+    let row = [];
+    for (let j = 0; j < numberofColumns; j++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
+
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced < numberOfBombs) {
+    //for now, it's possible that bombs are placed on top of each others
+    let randomRowIndex = Math.floor(Math.random()*numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random()*numberofColumns);
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+  }
+
+  return board;
 };
