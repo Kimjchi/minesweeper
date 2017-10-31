bf9967d58dd89cd37b947ca1c6e50e714e2316fa
kimjii <kimj77600@gmail.com>
2017-10-31 10:59:54 -0400

    Generate player board depending on the number of rows and columns

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,13 +1,15 @@
-const blankLine = '  |   |  ';
-const guessLine = '1 |   |  ';
-const bombLine = '  | B |  ';
+const generatePlayerBoard = (numberOfRows, numberofColumns) => {
+  let board = [];
+  for (let i = 0; i < numberOfRows; i++) {
+    let row = [];
+    for (let j = 0; j < numberofColumns; j++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
+};
 
-console.log('This is what an empty board would look like:');
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
-
-console.log('This is what a board with a guess and a bomb on it would look like:');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+const generateBombBoard = (numberOfRows, numberofColumns, numberOfBombs) => {
+  
+};
