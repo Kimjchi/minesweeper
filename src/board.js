249d2c5e3b50f7d241179bcd25f4c8daed44896a
kimjii <kimj77600@gmail.com>
2017-11-01 21:28:08 -0400

    We can play it on the terminal !

--- src/board.js
+++ src/board.js
@@ -1,25 +1,4 @@
-class Game {
-  constructor(numberOfRows, numberofColumns, numberOfBombs) {
-    this._board = new Board(numberOfRows, numberofColumns, numberOfBombs);
-  }
-
-  playMove(rowIndex, columnIndex) {
-    this._board.flipTile(rowIndex, columnIndex);
-    if (this._board.playerBoard[rowIndex][columnIndex] === 'B') {
-      console.log('GAME OVER');
-      this._board.print();
-    }
-    else if (!this._board.hasSafeTiles()) {
-      console.log('You WON. CONGRATULATIONS!');
-    }
-    else {
-      console.log('Current Board:');
-      this._board.print();
-    }
-  }
-}
-
-class Board {
+export class Board {
   constructor(numberOfRows, numberofColumns, numberOfBombs){
     this._numberOfBombs = numberOfBombs;
     this._numberOfTiles = numberOfRows * numberofColumns;
@@ -115,6 +94,3 @@ class Board {
   }
 
 }
-
-const g = new Game(3,3,3);
-g.playMove(0,0);
